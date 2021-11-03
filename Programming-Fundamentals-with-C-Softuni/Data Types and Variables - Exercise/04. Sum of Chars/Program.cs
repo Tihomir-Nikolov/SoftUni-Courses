62f249954092cdb425ddc3d6851a09420af68564
Tihomir-Nikolov <NikolovTihomir87@gmail.com>
2021-11-03 20:35:02 +0100

    Solution

---
+++ Programming-Fundamentals-with-C-Softuni/Data Types and Variables - Exercise/04. Sum of Chars/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _04._Sum_of_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int totalSum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                char input = char.Parse(Console.ReadLine());
+                input =(char)(int)input;
+                totalSum += input;
+            }
+            Console.WriteLine($"The sum equals: {totalSum}");
+        }
+    }
+}
