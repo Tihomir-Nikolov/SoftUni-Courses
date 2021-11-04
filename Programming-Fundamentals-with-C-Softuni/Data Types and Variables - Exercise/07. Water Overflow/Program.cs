83311762a2c41ab9d1f87cd3797d95f481b3f655
Tihomir-Nikolov <NikolovTihomir87@gmail.com>
2021-11-04 22:12:08 +0100

    Solution

---
+++ Programming-Fundamentals-with-C-Softuni/Data Types and Variables - Exercise/07. Water Overflow/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _07._Water_Overflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int CAPACYTY = 255;
+            int n = int.Parse(Console.ReadLine());
+            
+            int totalLiters = CAPACYTY;
+
+            for (int i = 0; i < n; i++)
+            {
+                int liters = int.Parse(Console.ReadLine());
+                if (totalLiters - liters >= 0 )
+                {
+                    totalLiters -= liters;
+                }
+                else
+                {
+                    Console.WriteLine($"Insufficient capacity!");
+                }
+                
+            }
+            int total = CAPACYTY - totalLiters;
+            Console.WriteLine(total);
+           
+        }
+    }
+}
