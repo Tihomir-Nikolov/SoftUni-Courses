7a9957e6ccc895e4a665963064f92fae7db4649e
Tihomir-Nikolov <NikolovTihomir87@gmail.com>
2021-11-05 21:04:08 +0100

    Exercise

---
+++ Programming-Fundamentals-with-C-Softuni/Data Types and Variables - Exercise/08. Beer Kegs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _08._Beer_Kegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double bigestKeg = double.MinValue;
+            string bigestmodel = "";
+            for (int i = 0; i < n; i++)
+            {
+                string model = Console.ReadLine();
+                float radius = float.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+
+                double capacity = Math.PI * Math.Pow(radius, 2) * height;
+                if (capacity > bigestKeg)
+                {
+                    bigestKeg = capacity;
+                    bigestmodel = model;
+                }
+            }
+            Console.WriteLine(bigestmodel);
+        }
+    }
+}
---
+++ Programming-Fundamentals-with-C-Softuni/Data Types and Variables - Exercise/09.Spice Must Flow/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Spice_Must_Flow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int WORKERS = 26;
+            const int COST_RESOURCE_PER_DAY = 10;
+
+            int gold = int.Parse(Console.ReadLine());
+            int totalGold = 0;
+            int couterOfDays = 0;
+
+            while (true)
+            {
+                if (gold < 100) break;
+
+                int startGold = gold;
+                couterOfDays++;
+                gold -= WORKERS;
+                totalGold += gold;
+                gold = startGold - COST_RESOURCE_PER_DAY;
+
+
+            }
+            totalGold -= WORKERS;
+            Console.WriteLine(couterOfDays);
+            Console.WriteLine(totalGold);
+        }
+    }
+}
+
