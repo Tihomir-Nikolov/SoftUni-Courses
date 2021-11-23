fe425018c84b1bb9beca46acc5d8f12df4da06f1
Tihomir-Nikolov <NikolovTihomir87@gmail.com>
2021-11-23 21:58:47 +0100

    solution
    homework

---
+++ Programming-Fundamentals-with-C-Softuni/18. Regular Expressions Exercise/02. Race/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02._Race
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string regexForDistance = @"[^\.\D+\W+\d+]?(?<distance>\d+?)";
+            string regexForName = @"(?<name>[A-Za-z]+)";
+            Dictionary<string, int> players = new Dictionary<string, int>();
+            int sum = 0;
+      
+            List<string> people = Console.ReadLine().Split(", ").ToList();
+            string input = Console.ReadLine();
+            while (input != "end of race")
+            {
+                MatchCollection nameMached = Regex.Matches(input, regexForName);
+                MatchCollection macheds = Regex.Matches(input, regexForDistance);
+                string currName = string.Join("", nameMached).ToString();
+                
+                foreach (Match match in macheds)
+                {
+                    sum += int.Parse(match.Groups["distance"].Value);
+   
+                }
+                if (people.Contains(currName) && !players.ContainsKey(currName))
+                {
+                    players.Add(currName, sum);
+                    
+                }
+                else if (players.ContainsKey(currName))
+                {
+                    players[currName] += sum;
+
+                }
+                sum = 0;
+                
+
+                input = Console.ReadLine();
+            }
+
+            var firstPlace = players.OrderByDescending(x => x.Value).Take(1);
+            var secondPlace = players.OrderByDescending(x => x.Value).Take(2).Skip(1);
+            var thirdPlace = players.OrderByDescending(x => x.Value).Take(3).Skip(2);
+            
+            foreach (var player in firstPlace)
+            {
+                Console.WriteLine($"1st place: {player.Key}");
+            }
+            
+            foreach (var player in secondPlace)
+            {
+                Console.WriteLine($"2nd place: {player.Key}");
+            }
+            foreach (var player in thirdPlace)
+            {
+                Console.WriteLine($"3rd place: {player.Key}");
+            }
+        }
+    }
+}
---
+++ Programming-Fundamentals-with-C-Softuni/18. Regular Expressions Exercise/03. SoftUni Bar Income/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03._SoftUni_Bar_Income
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"%(?<name>[A-Z][a-z]+)%[^%|$.]*<(?<product>[\w]+)>[^%|$.]*\|(?<quantity>[\d]+)\|[^%|$.]*?(?<price>[\d]+[.]?\d+)?\$";
+
+            double total = 0;
+            string input = Console.ReadLine();
+
+            while (input != "end of shift")
+            {
+
+                MatchCollection matches = Regex.Matches(input, pattern);
+                foreach (Match match in matches)
+                {
+                    string name = match.Groups["name"].Value;
+                    string product = match.Groups["product"].Value;
+                    int quantity = int.Parse(match.Groups["quantity"].Value);
+                    double price = double.Parse(match.Groups["price"].Value);
+                    double totalPrice = quantity * price;
+                    Console.WriteLine($"{name}: {product} - {totalPrice:f2}");
+                    total += totalPrice;
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Total income: {total:f2}");
+        }
+    }
+}
