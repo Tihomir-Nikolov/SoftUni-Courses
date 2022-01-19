fcba7cbcc41d18a2ae9d99e623581e153857d549
Tihomir-Nikolov <NikolovTihomir87@gmail.com>
2022-01-19 22:26:30 +0100

    Task Solution

---
+++ C# Advanced - януари 2022/Multidimensional Arrays - Lab/3. Primary Diagonal/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace _3._Primary_Diagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            int rows = input;
+            int cols = input;
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] rowsInput = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = rowsInput[col];
+                }
+            }
+            int sum = 0;
+            int count = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    
+                    if (count == col)
+                    {
+                        sum += matrix[row, col];
+                        count++;
+                        break;
+                    }
+                    
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ C# Advanced - януари 2022/Multidimensional Arrays - Lab/4. Symbol in Matrix/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace _4._Symbol_in_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            int rows = input;
+            int cols = input;
+            char[,] matrix = new char[rows, cols];
+            bool isFound = false;
+
+            for (int row = 0; row < rows; row++)
+            {
+                char[] simbols = Console.ReadLine().ToCharArray();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = simbols[col];
+                }
+            }
+            char simbol = char.Parse(Console.ReadLine());
+            int rowCounter = 0;
+            int colCounter = 0;
+            for (int row = 0; row < rows; row++)
+            {
+                
+                colCounter = 0;
+                for (int col = 0; col < cols; col++)
+                {
+                    if (simbol == matrix[row , col])
+                    {
+                        isFound = true;
+                        break;
+                    }                    
+                    colCounter++;
+                }
+                if (isFound)
+                {
+                    break;
+                }
+                rowCounter++;
+            }
+            if (isFound)
+            {
+                Console.WriteLine($"({rowCounter}, {colCounter})");
+            }
+            else
+            {
+                Console.WriteLine($"{simbol} does not occur in the matrix");
+            }
+        }
+    }
+}
