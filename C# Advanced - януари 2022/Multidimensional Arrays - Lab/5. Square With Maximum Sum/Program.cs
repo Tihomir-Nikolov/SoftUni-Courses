ca0a645c29e8e26c0050097c38ca6da2cf2bf3b1
Tihomir-Nikolov <NikolovTihomir87@gmail.com>
2022-01-22 23:41:19 +0100

    Task Solution

--- C# Advanced - януари 2022/Multidimensional Arrays - Lab/5. Square With Maximum Sum/Program.cs
+++ C# Advanced - януари 2022/Multidimensional Arrays - Lab/5. Square With Maximum Sum/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _5._Square_With_Maximum_Sum
 {
@@ -6,7 +7,53 @@ namespace _5._Square_With_Maximum_Sum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] input = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int rows = input[0];
+            int cols = input[1];
+            int[,] matrix = new int[rows, cols];
+            int bigestRows = 2;
+            int bigestCols = 2;
+            int indexRows = 0;
+            int indexCols = 0;
+            int bigestSum = int.MinValue;
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] rowsInput = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = rowsInput[col];
+                }
+            }
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    
+                    
+                        int sum = 0;
+                        if (bigestCols + col - 1 < cols && bigestRows + row - 1 < rows)
+                        {
+                            sum = matrix[row, col] + matrix[row + 1, col] + matrix[row, col + 1] + matrix[row + 1, col + 1];
+                        }
+                        if (sum > bigestSum)
+                        {
+                            bigestSum = sum;
+                            indexRows = row;
+                            indexCols = col;
+                        }
+                    
+                }
+            }
+            for (int row = indexRows; row < indexRows + bigestRows  ; row++)
+            {
+                for (int col = indexCols; col < indexCols + bigestCols ; col++)
+                {
+                    Console.Write($"{matrix[row,col]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine(bigestSum);
         }
     }
 }
---
+++ C# Advanced - януари 2022/Multidimensional Arrays - Lab/6. Jagged-Array Modification/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace _6._Jagged_Array_Modification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[][] jagged = new int[n][];
+            for (int rows = 0; rows < n; rows++)
+            {
+                int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                jagged[rows] = new int[input.Length];
+                for (int cols = 0; cols < input.Length; cols++)
+                {
+                   
+                    jagged[rows][cols] = input[cols];
+                }
+            }
+            string[] command = Console.ReadLine().Split();
+            
+
+            while (command[0] != "END")
+            {
+                int row = int.Parse(command[1]);
+                int col = int.Parse(command[2]);
+                int value = int.Parse(command[3]);
+                if (row < 0 || col < 0 || row >= jagged.Length || col >= jagged[row].Length )
+                {
+                    Console.WriteLine("Invalid coordinates");
+                }
+                else if (command[0] == "Add")
+                {
+                    jagged[row][col] += value;
+                }
+                else if (command[0] == "Subtract")
+                {
+                    jagged[row][col] -= value;
+                }
+
+                command = Console.ReadLine().Split();
+            }
+            for (int i = 0; i < jagged.Length; i++)
+            {
+               
+                Console.WriteLine($"{string.Join(" ", jagged[i])}");
+            }
+        }
+    }
+}
