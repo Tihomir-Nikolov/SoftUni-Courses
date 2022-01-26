f33a1d8af8d78cacfbd50805c44fc4ec62fae4b8
Tihomir-Nikolov <NikolovTihomir87@gmail.com>
2022-01-26 20:28:26 +0100

    Task Solution

--- C# Advanced - януари 2022/05. Sets and Dictionaries Advanced - Lab/02. Average Student Grades/Program.cs
+++ C# Advanced - януари 2022/05. Sets and Dictionaries Advanced - Lab/02. Average Student Grades/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _02._Average_Student_Grades
 {
@@ -6,7 +8,31 @@ namespace _02._Average_Student_Grades
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numberStudents = int.Parse(Console.ReadLine());
+            Dictionary<string, List<decimal>> studenGrades = new Dictionary<string, List<decimal>>();
+            
+            for (int i = 0; i < numberStudents; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string studentName = input[0];
+                decimal grades = decimal.Parse(input[1]);
+
+                if (!studenGrades.ContainsKey((studentName)))
+                {
+                    studenGrades.Add(studentName, new List<decimal>());
+                    
+                }
+                studenGrades[studentName].Add(grades);
+            }
+            foreach (var item in studenGrades)
+            {
+                Console.Write($"{item.Key} -> ");
+                foreach (var grade in item.Value)
+                {
+                    Console.Write($"{grade:f2} ");
+                }
+                Console.WriteLine($"(avg: {item.Value.Average():f2})");
+            }
         }
     }
 }
---
+++ C# Advanced - януари 2022/05. Sets and Dictionaries Advanced - Lab/04. Product Shop/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Product_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Dictionary<string, Dictionary<string, double>> shopAndProducts = new Dictionary<string, Dictionary<string, double>>();
+            while (input != "Revision")
+            {
+                string[] splited = input.Split(", ");
+                string shop = splited[0];
+                string product = splited[1];
+                double price = double.Parse(splited[2]);
+
+                if (!shopAndProducts.ContainsKey(shop))
+                {
+                    shopAndProducts.Add(shop, new Dictionary<string, double>());
+                }
+                shopAndProducts[shop].Add(product, price);
+
+                input = Console.ReadLine();
+            }
+            shopAndProducts = shopAndProducts.OrderBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value);
+            foreach (var shop in shopAndProducts)
+            {
+                Console.WriteLine($"{shop.Key}->");
+                foreach (var product in shop.Value)
+                {
+                    Console.WriteLine($"Product: {product.Key}, Price: {product.Value}");
+                }
+            }
+        }
+    }
+}
