3850e2e0a5f7ab29312e001d801df4dd091b40a0
Tihomir-Nikolov <NikolovTihomir87@gmail.com>
2022-01-26 21:52:35 +0100

    Task Solution

---
+++ C# Advanced - януари 2022/05.1 Sets/02. Parking Lot/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Parking_Lot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            HashSet<string> carNumber = new HashSet<string>();
+            while (input != "END")
+            {
+                string[] inputParams = input.Split(", ");
+                string direction = inputParams[0];
+                string carNumbers = inputParams[1];
+                if (direction == "IN")
+                {
+                    carNumber.Add(carNumbers);
+                }
+                else
+                {
+                    carNumber.Remove(carNumbers);
+                }
+
+                input = Console.ReadLine();
+            }
+            if (carNumber.Any())
+            {
+                foreach (var car in carNumber)
+                {
+                    Console.WriteLine(car);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}
---
+++ C# Advanced - януари 2022/05.1 Sets/03. SoftUni Party/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._SoftUni_Party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> vipGuests = new HashSet<string>();
+            HashSet<string> regularGuests = new HashSet<string>();
+            bool IsPartyTime = true;
+
+            string input = Console.ReadLine();
+            while (input != "END")
+            {
+                if (IsPartyTime)
+                {
+                    if (input == "PARTY")
+                    {
+                        IsPartyTime = false;
+                    }
+                }
+                if (!IsPartyTime)
+                {
+                        if (vipGuests.Any())
+                        {
+                            vipGuests.Remove(input);
+                        }
+                        if (regularGuests.Any())
+                        {
+                            regularGuests.Remove(input);
+                        }
+                }
+               else
+                {
+                    if (input.Substring(0, 1).Any(char.IsDigit))
+                    {
+                        vipGuests.Add(input);
+                    }
+                    else
+                    {
+                        regularGuests.Add(input);
+                    }
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(vipGuests.Count + regularGuests.Count);
+            foreach (var guest in vipGuests)
+            {
+                Console.WriteLine(guest);
+            }
+            foreach (var guest in regularGuests)
+            {
+                Console.WriteLine(guest);
+            }
+        }
+    }
+}
