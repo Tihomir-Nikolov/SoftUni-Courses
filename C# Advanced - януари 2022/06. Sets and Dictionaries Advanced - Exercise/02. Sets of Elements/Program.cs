1a6ab44ff02feefb4efec19fd6efdbd462a6baf3
Tihomir-Nikolov <NikolovTihomir87@gmail.com>
2022-01-27 21:43:44 +0100

    Task Solution

---
+++ C# Advanced - януари 2022/06. Sets and Dictionaries Advanced - Exercise/02. Sets of Elements/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Sets_of_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] lenght = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int firstSetLenght = lenght[0];
+            int secondtSetLenght = lenght[1];
+            HashSet<int> first = new HashSet<int>();
+            HashSet<int> second = new HashSet<int>();
+            for (int i = 0; i < firstSetLenght; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                first.Add(number);
+            }
+            for (int i = 0; i < secondtSetLenght; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                second.Add(number);
+            }
+            foreach (var number in first)
+            {
+                foreach (var numbers in second)
+                {
+                    if (number == numbers)
+                    {
+                        Console.Write($"{number} ");
+                    }
+                }
+            }
+        }
+    }
+}
