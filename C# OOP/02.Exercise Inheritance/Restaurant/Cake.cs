b7be8873a9c8209896995e88d025cae680796aef
Tihomir-Nikolov <NikolovTihomir87@gmail.com>
2022-02-27 21:17:56 +0100

    Problems for exercises and homework for the C# OOP

---
+++ C# OOP/02.Exercise Inheritance/Restaurant/Cake.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        private const decimal PriceCake = 5;
+        private const double CaloriesCake = 1000;
+        private const double CakeGrams = 250;
+        public Cake(string name) 
+            : base(name, PriceCake, CakeGrams, CaloriesCake)
+        {
+        }
+    }
+}
---
+++ C# OOP/02.Exercise Inheritance/Restaurant/Coffee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        private const double CoffeeMilliliters = 50;
+        private const decimal CoffeePrice = 3.50m;
+
+
+        public Coffee(string name, double coffeine) 
+            : base(name, CoffeePrice, CoffeeMilliliters)
+        {
+            this.Coffeine = coffeine;
+        }
+        public double Coffeine { get; set; }
+    }
+}
